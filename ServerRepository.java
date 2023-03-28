d395eb1d09d98df769b2b7dcbd300c5576bb5976
Aniruddh Upadhyay <66471180+AniruddhUpadhyay@users.noreply.github.com>
2023-03-28 13:34:20 +0530

    Create ServerRepository.java

---
+++ ServerRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ServerRepository extends MongoRepository<Server, String> {
+
+    Server findByNameContaining(String name);
+
+}
